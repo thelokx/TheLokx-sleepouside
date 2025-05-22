34972ccaae620d5e12f28e50b273855cc1d87471
Ricardo Salazar <therlokx@gmail.com>
2025-05-23 00:25:11 +0200

    up

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,21 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam('product');
 
-function addProductToCart(product) {
-  const carItems = getLocalStorage("so-cart") || [];
-  carItems.push(product);
-  setLocalStorage("so-cart", carItems);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
